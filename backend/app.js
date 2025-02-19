d3a3a7d07bed4d26ebdcaf0cf174d6974e1c32f3
bhavnapandit <pritybhavna2001@gmail.com>
2025-02-19 11:28:15 +0530

    starting backend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:12345@cluster0.8cjrn.mongodb.net/Blog?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .then(() => {
+    console.log("Connected to database");
+  });
