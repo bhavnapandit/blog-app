7737474305daa43b870c2722e44a6c91f64e0beb
bhavnapandit <pritybhavna2001@gmail.com>
2025-02-19 13:05:50 +0530

    added login and signup function

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import router from "./routes/userRoutes.js";
+
 const app = express();
+app.use(express.json());
+app.use("/api/user", router);
 
 mongoose
-    .connect(
-        "mongodb+srv://admin:12345@cluster0.8cjrn.mongodb.net/Blog?retryWrites=true&w=majority"
-    )
-    .then(() => {
-        app.listen(5000);
-    })
-    .then(() => {
-        console.log("Connected to database");
-    }).catch((err) => {
-       console.log(err);
-       
-    });
+  .connect(
+    "mongodb+srv://admin:12345@cluster0.8cjrn.mongodb.net/Blog?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => console.log("Database connection error:", err));
+
---
+++ backend/controllers/userController.js
@@ -0,0 +1,62 @@
+import User from "../model/User.js"
+import bcrypt from "bcrypt";
+
+export const getAllUsers = async (req, res, next) => {
+    let users;
+    try {
+        users = await User.find();
+
+    } catch (error) {
+        console.log(error);
+    }
+    if (!users) {
+        return res.status(404).json({ message: "No users found" });
+    }
+
+    return res.status(200).json({ users });
+}
+
+export const signUp = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+
+        const hashPassword = bcrypt.hashSync(password, 10)
+
+        const user = new User({
+            name,
+            email,
+            password: hashPassword,
+        });
+
+        await user.save();
+        res.status(201).json({ user });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error creating user" });
+    }
+};
+
+export const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ message: "Try signing up first!!" });
+        }
+
+        const isPassword = bcrypt.compareSync(password, user.password);
+        if (!isPassword) {
+            return res.status(400).jason({ message: "Password is incorrect" });
+        }
+        res.status(200).json({ user });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
