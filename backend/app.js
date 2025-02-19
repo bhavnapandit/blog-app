3711af86f54c639215013b45d64c632f73da2c00
bhavnapandit <pritybhavna2001@gmail.com>
2025-02-19 11:41:06 +0530

    added user model

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,15 @@ import mongoose from "mongoose";
 const app = express();
 
 mongoose
-  .connect(
-    "mongodb+srv://admin:12345@cluster0.8cjrn.mongodb.net/Blog?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    app.listen(5000);
-  })
-  .then(() => {
-    console.log("Connected to database");
-  });
+    .connect(
+        "mongodb+srv://admin:12345@cluster0.8cjrn.mongodb.net/Blog?retryWrites=true&w=majority"
+    )
+    .then(() => {
+        app.listen(5000);
+    })
+    .then(() => {
+        console.log("Connected to database");
+    }).catch((err) => {
+       console.log(err);
+       
+    });
